3f6a0546a6871b1c53f67fc476b4b20057bf38ba
“Simona-dev” <cu_simo@yahoo.com>
2021-03-31 15:58:30 +0300

    Tema

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,16 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World!");
+        System.out.println("Hello");
+        System.out.println("Simona");
+        System.out.println(255+27);
+        System.out.println(22/3f);
+        System.out.println(-5+8*6);
+        System.out.println((55+9) % 9 );
+        System.out.println(20+-3*5/8);
+        System.out.println(5+15/3*2-8%3);
+
+
 
     }
 }
